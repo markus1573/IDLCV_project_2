c6e32395154fa0c079fcb3e74b48d96afb35d9d3
Magdk01 <Magnusvinjebo@hotmail.com>
2025-10-08 14:56:49 +0200

    made datasets.py compatible with retarded win path naming

--- datasets.py
+++ datasets.py
@@ -5,17 +5,14 @@ from PIL import Image
 import torch
 from torchvision import transforms as T
 
+
 class FrameImageDataset(torch.utils.data.Dataset):
-    def __init__(self, 
-    root_dir='data/ufc10',
-    split='train', 
-    transform=None
-):
-        self.frame_paths = sorted(glob(f'{root_dir}/frames/{split}/*/*/*.jpg'))
-        self.df = pd.read_csv(f'{root_dir}/metadata/{split}.csv')
+    def __init__(self, root_dir="data/ufc10", split="train", transform=None):
+        self.frame_paths = sorted(glob(f"{root_dir}/frames/{split}/*/*/*.jpg"))
+        self.df = pd.read_csv(f"{root_dir}/metadata/{split}.csv")
         self.split = split
         self.transform = transform
-       
+
     def __len__(self):
         return len(self.frame_paths)
 
@@ -24,10 +21,11 @@ class FrameImageDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         frame_path = self.frame_paths[idx]
-        video_name = frame_path.split('/')[-2]
-        video_meta = self._get_meta('video_name', video_name)
-        label = video_meta['label'].item()
-        
+        frame_path = frame_path.replace("\\", "/")
+        video_name = frame_path.split("/")[-2]
+        video_meta = self._get_meta("video_name", video_name)
+        label = video_meta["label"].item()
+
         frame = Image.open(frame_path).convert("RGB")
 
         if self.transform:
@@ -39,47 +37,46 @@ class FrameImageDataset(torch.utils.data.Dataset):
 
 
 class FrameVideoDataset(torch.utils.data.Dataset):
-    def __init__(self, 
-    root_dir = 'data/ufc10', 
-    split = 'train', 
-    transform = None,
-    stack_frames = True
-):
+    def __init__(
+        self, root_dir="data/ufc10", split="train", transform=None, stack_frames=True
+    ):
 
-        self.video_paths = sorted(glob(f'{root_dir}/videos/{split}/*/*.avi'))
-        self.df = pd.read_csv(f'{root_dir}/metadata/{split}.csv')
+        self.video_paths = sorted(glob(f"{root_dir}/videos/{split}/*/*.avi"))
+        self.df = pd.read_csv(f"{root_dir}/metadata/{split}.csv")
         self.split = split
         self.transform = transform
         self.stack_frames = stack_frames
-        
+
         self.n_sampled_frames = 10
 
     def __len__(self):
         return len(self.video_paths)
-    
+
     def _get_meta(self, attr, value):
         return self.df.loc[self.df[attr] == value]
 
     def __getitem__(self, idx):
         video_path = self.video_paths[idx]
-        video_name = video_path.split('/')[-1].split('.avi')[0]
-        video_meta = self._get_meta('video_name', video_name)
-        label = video_meta['label'].item()
+        video_path = video_path.replace("\\", "/")
+        video_name = video_path.split("/")[-1].split(".avi")[0]
+        video_meta = self._get_meta("video_name", video_name)
+        label = video_meta["label"].item()
 
-        video_frames_dir = self.video_paths[idx].split('.avi')[0].replace('videos', 'frames')
+        video_frames_dir = (
+            self.video_paths[idx].split(".avi")[0].replace("videos", "frames")
+        )
         video_frames = self.load_frames(video_frames_dir)
 
         if self.transform:
             frames = [self.transform(frame) for frame in video_frames]
         else:
             frames = [T.ToTensor()(frame) for frame in video_frames]
-        
+
         if self.stack_frames:
             frames = torch.stack(frames).permute(1, 0, 2, 3)
 
-
         return frames, label
-    
+
     def load_frames(self, frames_dir):
         frames = []
         for i in range(1, self.n_sampled_frames + 1):
@@ -90,20 +87,29 @@ class FrameVideoDataset(torch.utils.data.Dataset):
         return frames
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from torch.utils.data import DataLoader
 
-    root_dir = 'data/ufc10'
+    root_dir = "data/ufc10"
 
-    transform = T.Compose([T.Resize((64, 64)),T.ToTensor()])
-    frameimage_dataset = FrameImageDataset(root_dir=root_dir, split='val', transform=transform)
-    framevideostack_dataset = FrameVideoDataset(root_dir=root_dir, split='val', transform=transform, stack_frames = True)
-    framevideolist_dataset = FrameVideoDataset(root_dir=root_dir, split='val', transform=transform, stack_frames = False)
+    transform = T.Compose([T.Resize((64, 64)), T.ToTensor()])
+    frameimage_dataset = FrameImageDataset(
+        root_dir=root_dir, split="val", transform=transform
+    )
+    framevideostack_dataset = FrameVideoDataset(
+        root_dir=root_dir, split="val", transform=transform, stack_frames=True
+    )
+    framevideolist_dataset = FrameVideoDataset(
+        root_dir=root_dir, split="val", transform=transform, stack_frames=False
+    )
 
-
-    frameimage_loader = DataLoader(frameimage_dataset,  batch_size=8, shuffle=False)
-    framevideostack_loader = DataLoader(framevideostack_dataset,  batch_size=8, shuffle=False)
-    framevideolist_loader = DataLoader(framevideolist_dataset,  batch_size=8, shuffle=False)
+    frameimage_loader = DataLoader(frameimage_dataset, batch_size=8, shuffle=False)
+    framevideostack_loader = DataLoader(
+        framevideostack_dataset, batch_size=8, shuffle=False
+    )
+    framevideolist_loader = DataLoader(
+        framevideolist_dataset, batch_size=8, shuffle=False
+    )
 
     # for frames, labels in frameimage_loader:
     #     print(frames.shape, labels.shape) # [batch, channels, height, width]
@@ -114,5 +120,6 @@ if __name__ == '__main__':
     #         print(frame.shape, labels.shape)# [batch, channels, height, width]
 
     for video_frames, labels in framevideostack_loader:
-        print(video_frames.shape, labels.shape) # [batch, channels, number of frames, height, width]
-            
+        print(
+            video_frames.shape, labels.shape
+        )  # [batch, channels, number of frames, height, width]
